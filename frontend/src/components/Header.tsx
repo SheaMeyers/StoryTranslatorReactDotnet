9f4b2bf14469486cafe59eda2b54cf6e47a4ef1c
Shea Meyers <shea.meyers@gmail.com>
2023-12-24 17:47:32 +0100

    Begin implementing styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import AppBar from '@mui/material/AppBar'
 import Button from '@mui/material/Button'
+import "../styling/Header.css"
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Button color="inherit">Login</Button>
+    <AppBar position="static" className="Header" >
+        <Button variant="outlined" className="Header__Button">Login</Button>
+        <Button variant="outlined" className="Header__Button">Sign Up</Button>
     </AppBar>
   );
 }
