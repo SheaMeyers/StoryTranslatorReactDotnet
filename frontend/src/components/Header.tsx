cca9f6bc59c94bbfd59570a02cec2daea8b58858
Shea Meyers <shea.meyers@gmail.com>
2023-12-24 16:25:01 +0100

    Display first nav bar

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Button color="inherit">Login</Button>
+    </AppBar>
+  );
+}
+
+export default Header;
