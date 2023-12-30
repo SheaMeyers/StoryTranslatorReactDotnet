47b8a89a88cd9b349fd0f7c5408dbb742303ce9f
Shea Meyers <shea.meyers@gmail.com>
2023-12-30 13:02:30 +0100

    Add language selector

--- frontend/src/components/Book.tsx
+++ frontend/src/components/Book.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react"
 import { getBooks } from "../apis/BookApi"
-import FormControl from "@mui/material/FormControl"
-import InputLabel from "@mui/material/InputLabel"
 import Select from "@mui/material/Select"
 import MenuItem from "@mui/material/MenuItem"
+import "../styling/Book.css"
 
 
 
@@ -12,6 +11,10 @@ const Book = () => {
 
   const [books, setBooks] = useState<string[]>([])
   const [selectedBook, setSelectedBook] = useState<string>('')
+  const [translateFrom, setTranslateFrom] = useState<string>('')
+  const [translateTo, setTranslateTo] = useState<string>('')
+
+  const languages = ['English', 'Spanish', 'French', 'German']
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -24,13 +27,34 @@ const Book = () => {
   return (
     <>
       <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
+        labelId="select-book-label"
+        id="select-book"
         value={selectedBook}
         label="Select Book"
+        className="BookSelector"
         onChange={(e) => setSelectedBook(e.target.value)}
       >
         {books.map(book => <MenuItem value={book}>{book}</MenuItem>)}
+      </Select>
+      <Select
+        labelId="translate-from-label"
+        id="translate-from"
+        value={translateFrom}
+        label="Select Book"
+        className="BookSelector"
+        onChange={(e) => setTranslateFrom(e.target.value)}
+      >
+        {languages.map(language => <MenuItem value={language}>{language}</MenuItem>)}
+      </Select>
+      <Select
+        labelId="translate-to-label"
+        id="translate-to"
+        value={translateTo}
+        label="Select Book"
+        className="BookSelector"
+        onChange={(e) => setTranslateTo(e.target.value)}
+      >
+        {languages.map(language => <MenuItem value={language}>{language}</MenuItem>)}
       </Select>
     </>
   )
